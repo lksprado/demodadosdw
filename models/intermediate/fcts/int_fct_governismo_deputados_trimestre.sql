18d5ca5f3084bfb35785d714c24775fa53853b7d
lksprado <lks-prado@live.com>
2025-11-28 01:00:49 -0300

    fix: trimestre

--- models/intermediate/fcts/int_fct_governismo_deputados_trimestre.sql
+++ models/intermediate/fcts/int_fct_governismo_deputados_trimestre.sql
@@ -13,10 +13,8 @@ fato as (
     select 
     t2.sk_parlamentar,
     t2.casa,
-    case 
-        when t1.data_trimestre = date '2023-03-31' then date '2023-04-01'
-        else t1.data_trimestre
-    end as data_trimestre,
+    date_trunc('quarter', t1.data_trimestre - interval '1 month')::date as data_trimestre,
+    -- date_trunc('quarter', t1.data_trimestre)::date as data_trimestre,
     t1.perc_governismo_trimestre,
     t1.data_carga
     from {{ ref("stg_radarcongresso__governismo_deputados") }} t1
--- models/marts/mrt_governismo_parlamentares.sql
+++ models/marts/mrt_governismo_parlamentares.sql
@@ -2,27 +2,33 @@
     tags=["mrt","radar","parlamentar"]
 ) }}
 
-
 WITH
-dim_dep AS (
-    SELECT * FROM {{ ref('int_dim_deputados')}}
+dim_parl AS (
+    SELECT * FROM {{ ref('int_dim_parlamentares')}}
 ),
 
-fct_gov_total AS (
+fct_gov_deps AS (
     SELECT * FROM {{ ref('int_fct_governismo_deputados_total')}}
 ),
-
+fct_gov_sens AS (
+    SELECT * FROM {{ ref('int_fct_governismo_senadores_total')}}
+),
+fct_union as (
+    select * from fct_gov_deps
+    union
+    select * from fct_gov_sens
+),
 tab AS (
     SELECT
         t1.sk_parlamentar,
+        t1.casa,
         t1.nome_eleitoral,
         t1.partido,
         t1.uf_representacao,
         t2.perc_governismo
-    FROM dim_dep AS t1
-    INNER JOIN fct_gov_total AS t2
+    FROM dim_parl AS t1
+    INNER JOIN fct_union AS t2
         ON t1.sk_parlamentar = t2.sk_parlamentar
     where t2.perc_governismo is not null
 )
-
 SELECT * FROM tab
